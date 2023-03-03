967ba5d6445365b28bec56abf3c0964caac306ac
Samir Nayakawadi <samnayakawadi@gmail.com>
2023-03-03 17:41:35 +0530

    Changed the Get in Touch Image

--- src/components/menu/home/components/GetInTouch.jsx
+++ src/components/menu/home/components/GetInTouch.jsx
@@ -1,17 +1,18 @@
+import getInTouchImage from "./images/getInTouch/contact-trans.png"
 const GetInTouch = () => {
     return (
         <div className="px-80" id="contact">
             <div className="card rounded-3xl bg-base-100 shadow-xl">
-                <div className="flex flex-row justify-center items-center gap-5">
-                    <div className="basis-6/12 p-10">
+                <div className="flex flex-row justify-center items-center">
+                    <div className="basis-7/12 pl-10">
                         <h1 className="text-4xl font-bold">Serving Our Customers</h1>
                         <p className="pt-6">Our goal is to help customers optimize the use of YouTestMe, by providing a premium onboarding experience. At YouTestMe, we strive to integrate customer service into every aspect of our business.</p>
                         <a href="mailto:meghsikshak@cdac.in" alt="">
                             <button className="btn btn-primary mt-6">Get in Touch</button>
                         </a>
                     </div>
-                    <div className="basis-6/12">
-                        <img className="w-full" src="https://img.freepik.com/free-photo/business-woman-checkered-jacket-with-smile-while-sitting-desk-her-office_197531-10661.jpg?w=1380&t=st=1677497665~exp=1677498265~hmac=439f3bb7be92a946ee587f4ea306f14135a2300713d0f71c8b710f5ca9db1a83" alt="" />
+                    <div className="basis-5/12 flex flex-row justify-center items-center">
+                        <img className="max-w-xs" src={getInTouchImage} alt="" />
                     </div>
                 </div>
             </div>
