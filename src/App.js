bb6fa5c7df412a130990695c60e17a0354a3ad3e
Samir Nayakawadi <samnayakawadi@gmail.com>
2023-03-20 14:56:26 +0530

    Automatic Dark Mode Commented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Footer from "./components/footer/Footer";
 import Home from "./components/menu/home/Home";
 import Navbar from "./components/navbar/Navbar";
@@ -7,11 +7,11 @@ function App() {
 
   const [theme, setTheme] = useState("lofi")
 
-  useEffect(() => {
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      setTheme("business")
-    }
-  }, [])
+  // useEffect(() => {
+  //   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  //     setTheme("business")
+  //   }
+  // }, [])
 
   return (
     <div data-theme={theme}>
